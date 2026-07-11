26eb7acce2156a8266e031a6f0265f9ef2a2cbca
Chandra2004 <chandratriantomo123@gmail.com>
2026-07-11 11:01:59 +0700

    chore: initialize application structure with filesystem, Livewire, and PHP configuration files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->web(append: [
             \App\Http\Middleware\ShareNotifications::class,
-            \App\Http\Middleware\NgrokBypass::class,
         ]);
 
         $middleware->alias([
